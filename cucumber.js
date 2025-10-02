4d8722893627796932b6c58b5ec7934453c4ef81
stoychoenchev <stoychoenchev@gmail.com>
2025-10-02 11:59:21 +0300

    changes again in CI job and in the cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,10 +1,8 @@
 module.exports = {
 default: {
   requireModule: ['ts-node/register'],
-  require: ['features/steps/**/*.ts'],
+  require: ['features/steps/*.ts'],
   format: ['progress'],
-  publishQuiet: true,
-  failFast: false,
   parallel: 1,
   paths: ['features/*.feature'],
 }
